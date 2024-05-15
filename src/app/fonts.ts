e80bbf64f6737b76191d2e5c08864e7b4cb21ba5
Artyom Ulanchik <artyom.u@outlook.com>
2024-05-15 10:17:29 +0100

    project init

---
+++ src/app/fonts.ts
@@ -0,0 +1,15 @@
+import { Inter, Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export { inter, poppins };
